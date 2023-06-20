20be20456a6b18bd30c803e6bdc7db272f0b139f
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-06-20 13:36:06 +0100

    Retrofit

--- app/src/main/java/com/example/apidatafetchusingretrofitlibrary/MainActivity.kt
+++ app/src/main/java/com/example/apidatafetchusingretrofitlibrary/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.apidatafetchusingretrofitlibrary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+
+const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getMyData()
+    }
+
+    private fun getMyData() {
+        val retrofitBuilder = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(ApiInterface::class.java)
+
+        val retrofitData = retrofitBuilder.getData()
+
+        retrofitData.enqueue(object : Callback<List<MyDataItem>?> {
+            override fun onResponse(
+                call: Call<List<MyDataItem>?>,
+                response: Response<List<MyDataItem>?>
+
+            ) {
+                val myStringBuilder = StringBuilder()
+                val responseBody = response.body()!!
+                for(myData in responseBody){
+                    myStringBuilder.append(myData.id)
+                    myStringBuilder.append("\n")
+                }
+
+                val display = findViewById<TextView>(R.id.txtId)
+                display.text = myStringBuilder
+            }
+
+            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
+                Log.d("MainActivity", "onFailure: "+ t.message)
+            }
+        })
+
     }
 }
---
+++ app/src/main/java/com/example/apidatafetchusingretrofitlibrary/MyData.kt
@@ -0,0 +1,3 @@
+package com.example.apidatafetchusingretrofitlibrary
+
+class MyData : ArrayList<MyDataItem>()
